9967c09f5e56602c301d8e1dcbcc88668d0f2ef5
callred88 <callred88@outlook.com>
2023-07-24 17:52:40 -0500

    I have started coding for Selenium to scrape the page.

--- Webscraping.py
+++ Webscraping.py
@@ -8,7 +8,18 @@ file = open("laptops.csv", "w")
 writer = csv.writer(file)
 writer.writerow(["id", "name", "price", "specifications", "number of reviews"])
 
-browser_driver = Service("C:\Users\Callr\Downloads\chromedriver_win32")
+browser_driver = Service("Users\Callr\Downloads\chromedriver_win32")
 scraper = webdriver.Chrome(service=browser_driver)
 
+#Here we grab the site and first page. 
 scraper.get("https://webscraper.io/test-sites/e-commerce/static/computers/laptops")
+
+data= []
+
+itemCards = scraper.find_elements(By.CLASS_NAME, "thumbnail")
+for item in itemCards:
+    name = item.find_element(By.CLASS_NAME, "title").get_attribute("title")
+
+
+
+
